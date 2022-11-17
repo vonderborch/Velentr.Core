e0f3f7003ba5fe1b01cbe478e67b00c2a10a59a1
Christian Webber <ricky.webber@hotmail.com>
2022-11-17 14:03:07 -0500

    Clamp has default values for min and max

--- source/Velentr.Core/Helpers/Math/MathHelpers.cs
+++ source/Velentr.Core/Helpers/Math/MathHelpers.cs
@@ -20,6 +20,20 @@
         }
 
         /// <summary>
+        ///     Tests if objects are considered equal.
+        /// </summary>
+        /// <param name="a">            Double to be compared. </param>
+        /// <param name="b">            Double to be compared. </param>
+        /// <param name="precision">    Double to be compared. </param>
+        /// <returns>
+        ///     True if the objects are considered equal, false if they are not.
+        /// </returns>
+        public static bool Equals(float a, float b, float precision)
+        {
+            return System.Math.Abs(a - b) <= precision;
+        }
+
+        /// <summary>
         ///     Clamp the given value.
         /// </summary>
         /// <param name="value">    The value. </param>
@@ -28,7 +42,7 @@
         /// <returns>
         ///     A double.
         /// </returns>
-        public static double Clamp(double value, double minValue, double maxValue)
+        public static double Clamp(double value, double minValue = double.MinValue, double maxValue = double.MaxValue)
         {
             return value < minValue
                        ? minValue
@@ -46,7 +60,7 @@
         /// <returns>
         ///     A int.
         /// </returns>
-        public static int Clamp(int value, int minValue, int maxValue)
+        public static int Clamp(int value, int minValue = int.MinValue, int maxValue = int.MaxValue)
         {
             return value < minValue
                        ? minValue
@@ -64,7 +78,7 @@
         /// <returns>
         ///     A float.
         /// </returns>
-        public static float Clamp(float value, float minValue, float maxValue)
+        public static float Clamp(float value, float minValue = float.MinValue, float maxValue = float.MaxValue)
         {
             return value < minValue
                        ? minValue
@@ -82,7 +96,7 @@
         /// <returns>
         ///     A long.
         /// </returns>
-        public static long Clamp(long value, long minValue, long maxValue)
+        public static long Clamp(long value, long minValue = long.MinValue, long maxValue = long.MaxValue)
         {
             return value < minValue
                        ? minValue
