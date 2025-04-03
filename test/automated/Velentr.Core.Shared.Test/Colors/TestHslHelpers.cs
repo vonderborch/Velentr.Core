66044e4e9396c04a5fdbb77400d05319af661f99
Christian Webber <ricky.webber@hotmail.com>
2025-04-03 17:05:34 -0400

    HSL in progress

--- test/automated/Velentr.Core.Shared.Test/Colors/TestHslHelpers.cs
+++ test/automated/Velentr.Core.Shared.Test/Colors/TestHslHelpers.cs
@@ -7,45 +7,49 @@ namespace Velentr.Core.Test.Velentr.Core.Shared.Test.Colors
         [Test]
         public void TestDoublesConvertHslToRgb_ValidHsl()
         {
-            double hue = 0.05, saturation = 0.9, lightness = 0.6;
+            int hue = 18;
+            double saturation = 0.9, lightness = 0.6;
             double red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(1.0, red, 0.01);
-            Assert.AreEqual(0.34, green, 0.01);
-            Assert.AreEqual(0.2, blue, 0.01);
+            Assert.That(red, Is.EqualTo(1.0).Within(0.01));
+            Assert.That(green, Is.EqualTo(0.35).Within(0.01));
+            Assert.That(blue, Is.EqualTo(0.2).Within(0.01));
         }
 
         [Test]
         public void TestDoublesConvertHslToRgb_MinHsl()
         {
-            double hue = 0.0, saturation = 0.0, lightness = 0.0;
+            int hue = 0;
+            double saturation = 0.0, lightness = 0.0;
             double red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(0.0, red, 0.01);
-            Assert.AreEqual(0.0, green, 0.01);
-            Assert.AreEqual(0.0, blue, 0.01);
+            Assert.That(red, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(green, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(blue, Is.EqualTo(0.0).Within(0.01));
         }
 
         [Test]
         public void TestDoublesConvertHslToRgb_MaxHsl()
         {
-            double hue = 1.0, saturation = 1.0, lightness = 1.0;
+            int hue = 360;
+            double saturation = 1.0, lightness = 1.0;
             double red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(1.0, red, 0.01);
-            Assert.AreEqual(1.0, green, 0.01);
-            Assert.AreEqual(1.0, blue, 0.01);
+            Assert.That(red, Is.EqualTo(1.0).Within(0.01));
+            Assert.That(green, Is.EqualTo(1.0).Within(0.01));
+            Assert.That(blue, Is.EqualTo(1.0).Within(0.01));
         }
 
         [Test]
         public void TestDoublesConvertHslToRgb_MixedHsl()
         {
-            double hue = 0.6, saturation = 0.5, lightness = 0.5;
+            int hue = 216;
+            double saturation = 0.5, lightness = 0.5;
             double red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(0.25, red, 0.01);
-            Assert.AreEqual(0.75, green, 0.01);
-            Assert.AreEqual(0.75, blue, 0.01);
+            Assert.That(red, Is.EqualTo(0.25).Within(0.01));
+            Assert.That(green, Is.EqualTo(0.75).Within(0.01));
+            Assert.That(blue, Is.EqualTo(0.75).Within(0.01));
         }
         [Test]
         public void TestBytesConvertHslToRgb_ValidHsl()
@@ -54,9 +58,9 @@ namespace Velentr.Core.Test.Velentr.Core.Shared.Test.Colors
             double saturation = 0.9, lightness = 0.6;
             byte red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(255, red);
-            Assert.AreEqual(87, green);
-            Assert.AreEqual(51, blue);
+            Assert.That(red, Is.EqualTo(255));
+            Assert.That(red, Is.EqualTo(87));
+            Assert.That(red, Is.EqualTo(51));
         }
 
         [Test]
@@ -66,9 +70,9 @@ namespace Velentr.Core.Test.Velentr.Core.Shared.Test.Colors
             double saturation = 0.0, lightness = 0.0;
             byte red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(0, red);
-            Assert.AreEqual(0, green);
-            Assert.AreEqual(0, blue);
+            Assert.That(red, Is.EqualTo(0));
+            Assert.That(green, Is.EqualTo(0));
+            Assert.That(blue, Is.EqualTo(0));
         }
 
         [Test]
@@ -78,9 +82,9 @@ namespace Velentr.Core.Test.Velentr.Core.Shared.Test.Colors
             double saturation = 1.0, lightness = 1.0;
             byte red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(255, red);
-            Assert.AreEqual(255, green);
-            Assert.AreEqual(255, blue);
+            Assert.That(red, Is.EqualTo(255));
+            Assert.That(green, Is.EqualTo(255));
+            Assert.That(blue, Is.EqualTo(255));
         }
 
         [Test]
@@ -90,9 +94,256 @@ namespace Velentr.Core.Test.Velentr.Core.Shared.Test.Colors
             double saturation = 0.5, lightness = 0.5;
             byte red, green, blue;
             HslHelpers.ConvertHslToRgb(hue, saturation, lightness, out red, out green, out blue);
-            Assert.AreEqual(64, red);
-            Assert.AreEqual(191, green);
-            Assert.AreEqual(191, blue);
+            Assert.That(red, Is.EqualTo(64));
+            Assert.That(green, Is.EqualTo(191));
+            Assert.That(blue, Is.EqualTo(191));
+        }
+        [Test]
+        public void TestConvertHslToRgb_ValidHsl()
+        {
+            int hue = 18;
+            double saturation = 0.9, lightness = 0.6;
+            (double red, double green, double blue) = HslHelpers.ConvertHslToRgb(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(1.0).Within(0.01));
+            Assert.That(green, Is.EqualTo(0.35).Within(0.01));
+            Assert.That(blue, Is.EqualTo(0.2).Within(0.01));
+        }
+
+        [Test]
+        public void TestConvertHslToRgb_MinHsl()
+        {
+            int hue = 0;
+            double saturation = 0.0, lightness = 0.0;
+            (double red, double green, double blue) = HslHelpers.ConvertHslToRgb(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(green, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(blue, Is.EqualTo(0.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestConvertHslToRgb_MaxHsl()
+        {
+            int hue = 360;
+            double saturation = 1.0, lightness = 1.0;
+            (double red, double green, double blue) = HslHelpers.ConvertHslToRgb(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(1.0).Within(0.01));
+            Assert.That(green, Is.EqualTo(1.0).Within(0.01));
+            Assert.That(blue, Is.EqualTo(1.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestConvertHslToRgb_MixedHsl()
+        {
+            int hue = 216;
+            double saturation = 0.5, lightness = 0.5;
+            (double red, double green, double blue) = HslHelpers.ConvertHslToRgb(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(0.25).Within(0.01));
+            Assert.That(green, Is.EqualTo(0.75).Within(0.01));
+            Assert.That(blue, Is.EqualTo(0.75).Within(0.01));
+        }
+        
+        [Test]
+        public void TestConvertHslToRgbBytes_ValidHsl()
+        {
+            int hue = 18;
+            double saturation = 0.9, lightness = 0.6;
+            (byte red, byte green, byte blue) = HslHelpers.ConvertHslToRgbBytes(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(255));
+            Assert.That(green, Is.EqualTo(87));
+            Assert.That(blue, Is.EqualTo(51));
+        }
+
+        [Test]
+        public void TestConvertHslToRgbBytes_MinHsl()
+        {
+            int hue = 0;
+            double saturation = 0.0, lightness = 0.0;
+            (byte red, byte green, byte blue) = HslHelpers.ConvertHslToRgbBytes(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(0));
+            Assert.That(green, Is.EqualTo(0));
+            Assert.That(blue, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestConvertHslToRgbBytes_MaxHsl()
+        {
+            int hue = 360;
+            double saturation = 1.0, lightness = 1.0;
+            (byte red, byte green, byte blue) = HslHelpers.ConvertHslToRgbBytes(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(255));
+            Assert.That(green, Is.EqualTo(255));
+            Assert.That(blue, Is.EqualTo(255));
+        }
+
+        [Test]
+        public void TestConvertHslToRgbBytes_MixedHsl()
+        {
+            int hue = 216;
+            double saturation = 0.5, lightness = 0.5;
+            (byte red, byte green, byte blue) = HslHelpers.ConvertHslToRgbBytes(hue, saturation, lightness);
+            Assert.That(red, Is.EqualTo(64));
+            Assert.That(green, Is.EqualTo(191));
+            Assert.That(blue, Is.EqualTo(191));
+        }
+        [Test]
+        public void TestFromBytesConvertRgbToHsl_ValidRgb()
+        {
+            byte red = 255, green = 87, blue = 51;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(18));
+            Assert.That(saturation, Is.EqualTo(0.9).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.6).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromBytesConvertRgbToHsl_MinRgb()
+        {
+            byte red = 0, green = 0, blue = 0;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromBytesConvertRgbToHsl_MaxRgb()
+        {
+            byte red = 255, green = 255, blue = 255;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(1.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromBytesConvertRgbToHsl_MixedRgb()
+        {
+            byte red = 64, green = 191, blue = 191;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(180));
+            Assert.That(saturation, Is.EqualTo(0.5).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.5).Within(0.01));
+        }
+        
+        [Test]
+        public void TestFromDoublesConvertRgbToHsl_ValidRgb()
+        {
+            double red = 1.0, green = 0.34, blue = 0.2;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(18));
+            Assert.That(saturation, Is.EqualTo(0.9).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.6).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromDoublesConvertRgbToHsl_MinRgb()
+        {
+            double red = 0.0, green = 0.0, blue = 0.0;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromDoublesConvertRgbToHsl_MaxRgb()
+        {
+            double red = 1.0, green = 1.0, blue = 1.0;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(1.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromDoublesConvertRgbToHsl_MixedRgb()
+        {
+            double red = 0.25, green = 0.75, blue = 0.75;
+            (int hue, double saturation, double lightness) = HslHelpers.ConvertRgbToHsl(red, green, blue);
+            Assert.That(hue, Is.EqualTo(180));
+            Assert.That(saturation, Is.EqualTo(0.5).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.5).Within(0.01));
+        }
+        
+        [Test]
+        public void TestFromBytesOutConvertRgbToHsl_ValidRgb()
+        {
+            byte red = 255, green = 87, blue = 51;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(18));
+            Assert.That(saturation, Is.EqualTo(0.9).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.6).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromBytesOutConvertRgbToHsl_MinRgb()
+        {
+            byte red = 0, green = 0, blue = 0;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromBytesOutConvertRgbToHsl_MaxRgb()
+        {
+            byte red = 255, green = 255, blue = 255;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(1.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestFromBytesOutConvertRgbToHsl_MixedRgb()
+        {
+            byte red = 64, green = 191, blue = 191;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(180));
+            Assert.That(saturation, Is.EqualTo(0.5).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.5).Within(0.01));
+        }
+        
+        
+        [Test]
+        public void TestConvertRgbToHsl_ValidRgb()
+        {
+            double red = 1.0, green = 0.34, blue = 0.2;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(18));
+            Assert.That(saturation, Is.EqualTo(0.9).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.6).Within(0.01));
+        }
+
+        [Test]
+        public void TestConvertRgbToHsl_MinRgb()
+        {
+            double red = 0.0, green = 0.0, blue = 0.0;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestConvertRgbToHsl_MaxRgb()
+        {
+            double red = 1.0, green = 1.0, blue = 1.0;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(0));
+            Assert.That(saturation, Is.EqualTo(0.0).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(1.0).Within(0.01));
+        }
+
+        [Test]
+        public void TestConvertRgbToHsl_MixedRgb()
+        {
+            double red = 0.25, green = 0.75, blue = 0.75;
+            HslHelpers.ConvertRgbToHsl(red, green, blue, out int hue, out double saturation, out double lightness);
+            Assert.That(hue, Is.EqualTo(180));
+            Assert.That(saturation, Is.EqualTo(0.5).Within(0.01));
+            Assert.That(lightness, Is.EqualTo(0.5).Within(0.01));
         }
     }
 }
