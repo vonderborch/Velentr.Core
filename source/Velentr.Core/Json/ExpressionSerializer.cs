b3b533a2b80a683a2103e61059574ab8a754fd65
Christian Webber <ricky.webber@hotmail.com>
2025-05-19 17:36:38 -0400

    3.2.0.0

---
+++ source/Velentr.Core/Json/ExpressionSerializer.cs
@@ -0,0 +1,31 @@
+using System.Linq.Expressions;
+using Serialize.Linq.Serializers;
+
+namespace Velentr.Core.Json;
+
+public class ExpressionSerializer<T> where T : Delegate
+{
+    public static ExpressionSerializer Serializer = new(new JsonSerializer());
+
+    /// <summary>
+    ///     Deserializes a Base64 encoded string into an expression of type T.
+    /// </summary>
+    /// <param name="serializedExpression">The Base64 encoded string to deserialize.</param>
+    /// <returns>The deserialized expression of type T, or null if the deserialization fails.</returns>
+    public static Expression<T> DeserializeExpression(string serializedExpression)
+    {
+        Expression? expression = Serializer.DeserializeText(serializedExpression);
+        return (Expression<T>)expression;
+    }
+
+    /// <summary>
+    ///     Serializes an expression of type T into a Base64 encoded string.
+    /// </summary>
+    /// <param name="expression">The expression to serialize.</param>
+    /// <returns>A Base64 encoded string representing the serialized expression.</returns>
+    public static string SerializeExpression(Expression<T> expression)
+    {
+        var expressionString = Serializer.SerializeText(expression);
+        return expressionString;
+    }
+}
---
+++ test/automated/Velentr.Core.Shared.Test/Json/TestExpressionSerializer.cs
@@ -0,0 +1,51 @@
+using System.Linq.Expressions;
+using Velentr.Core.Json;
+
+namespace Velentr.Core.Test.Json
+{
+    [TestFixture]
+    public class TestExpressionSerializer
+    {
+        [Test]
+        public void TestSerializeExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression = x => x + 1;
+
+            // Act
+            var serializedExpression = ExpressionSerializer<Func<int, int>>.SerializeExpression(expression);
+
+            // Assert
+            Assert.That(serializedExpression, Is.Not.Null);
+            Assert.That(serializedExpression, Is.Not.Empty);
+        }
+
+        [Test]
+        public void TestDeserializeExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression = x => x + 1;
+            var serializedExpression = ExpressionSerializer<Func<int, int>>.SerializeExpression(expression);
+
+            // Act
+            var deserializedExpression = ExpressionSerializer<Func<int, int>>.DeserializeExpression(serializedExpression);
+
+            // Assert
+            Assert.That(deserializedExpression, Is.Not.Null);
+            Assert.That(deserializedExpression.ToString(), Is.EqualTo(expression.ToString()));
+        }
+
+        [Test]
+        public void TestInvalidDeserialization()
+        {
+            // Arrange
+            var invalidSerializedExpression = "invalid_base64_string";
+
+            // Act & Assert
+            Assert.That(() =>
+            {
+                ExpressionSerializer<Func<int, int>>.DeserializeExpression(invalidSerializedExpression);
+            }, Throws.Exception);
+        }
+    }
+}
